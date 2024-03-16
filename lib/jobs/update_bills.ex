04e4d173748986f8238bb7e124b399305b75dad5
Dickson <dicksonmelo@gmail.com>
2024-03-16 06:57:24 -0300

    chore: add more information and run dive function only when there's a new reference month

--- lib/jobs/update_bills.ex
+++ lib/jobs/update_bills.ex
@@ -1,15 +1,42 @@
 defmodule PayCosern.Jobs.UpdateBills do
+  require Logger
   use Oban.Worker
+  alias PayCosern.Repo.Bills
 
   def perform(_job) do
-    data = PayCosern.dive()
+    Logger.info("\nRunning update_bills cronjob.\n")
 
-    case data do
-      {:error, _reason, message} ->
-        {:error, message}
+    today_reference_month = Timex.now() |> Timex.format!("{0M}/{YYYY}")
 
-      data ->
-        data
+    reference_month =
+      case PayCosern.Query.last_bill() do
+        %Bills{reference_month: reference_month} ->
+          reference_month
+
+        something ->
+          something
+      end
+
+    if today_reference_month == reference_month do
+      Logger.info("""
+      \n
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        ~~~> Data is updated. Last bill reference month: #{reference_month}\n
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        )
+      """)
+
+      {:ok, :updated_data}
+    else
+      data = PayCosern.dive()
+
+      case data do
+        {:error, _reason, message} ->
+          {:error, message}
+
+        data ->
+          data
+      end
     end
   end
 end
