e03e6e7e338468e8d64169a95940c098e2e979da
Dickson <dicksonmelo@gmail.com>
2024-03-18 21:01:46 -0300

    chore: add docs kk

--- lib/pay_cosern/gleam.ex
+++ lib/pay_cosern/gleam.ex
@@ -1,4 +1,12 @@
 defmodule PayCosern.Gleam do
+  @moduledoc """
+  This module exists to create a smooth interoperability between Elixir and Gleam.
+  """
+
+  @doc """
+  This function validates the structure of a bill. 
+  """
+  @spec validate_structure(map :: map()) :: {String.t(), map()}
   def validate_structure(map) do
     :pay_cosern.validate_bill_structure(map)
   end
